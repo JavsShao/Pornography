c13b3f4b946b6bdc6307ee21db7a4a913c11f0f1
hamdi <javs_shao@163.com>
2018-11-17 17:46:04 +0800

    初始化

--- run.py
+++ run.py
@@ -5,3 +5,47 @@ from collections import namedtuple
 from PIL import Image
 
 
+class Nude(object):
+
+    Skin = namedtuple("Skin", "id skin region xy")
+
+    def __init__(self, path_or_image):
+        # 若 path_or_image 为 Image.Image 类型的实例，直接赋值
+        if isinstance(path_or_image, Image.Image):
+            self.image = path_or_image
+        # 若 path_or_image 为 str 类型的实例，打开图片
+        elif isinstance(path_or_image, str):
+            self.image = Image.open(path_or_image)
+
+        # 获得图片所有颜色通道
+        bands = self.image.getbands()
+        # 判断是否为单通道图片（也即灰度图），是则将灰度图转换为 RGB 图
+        if len(bands) == 1:
+            # 新建相同大小的 RGB 图像
+            new_img = Image.new("RGB", self.image.size)
+            # 拷贝灰度图 self.image 到 RGB图 new_img.paste （PIL 自动进行颜色通道转换）
+            new_img.paste(self.image)
+            f = self.image.filename
+            # 替换 self.image
+            self.image = new_img
+            self.image.filename = f
+
+        # 存储对应图像所有像素的全部 Skin 对象
+        self.skin_map = []
+        # 检测到的皮肤区域，元素的索引即为皮肤区域号，元素都是包含一些 Skin 对象的列表
+        self.detected_regions = []
+        # 元素都是包含一些 int 对象（区域号）的列表
+        # 这些元素中的区域号代表的区域都是待合并的区域
+        self.merge_regions = []
+        # 整合后的皮肤区域，元素的索引即为皮肤区域号，元素都是包含一些 Skin 对象的列表
+        self.skin_regions = []
+        # 最近合并的两个皮肤区域的区域号，初始化为 -1
+        self.last_from, self.last_to = -1, -1
+        # 色情图像判断结果
+        self.result = None
+        # 处理得到的信息
+        self.message = None
+        # 图像宽高
+        self.width, self.height = self.image.size
+        # 图像总像素
+        self.total_pixels = self.width * self.height
